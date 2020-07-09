4c6281ee9ba6f32d9db2376869752fcd99df75c2
urielsb <uriel_sbarc@hotmail.com>
2020-07-08 23:20:20 -0500

    Add Visit entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,12 +1,25 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jt on 7/13/18.
  */
 public class Owner extends Person {
 
 	/**
-	 * 
+	 * Serial
 	 */
 	private static final long serialVersionUID = 6058308647075194395L;
+	
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
 }
