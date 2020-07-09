dca8c442821fe3ff21fbaeb84eae3799d19d8d7e
urielsb <uriel_sbarc@hotmail.com>
2020-07-08 23:33:01 -0500

    Add contact info to owner

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -13,6 +13,10 @@ public class Owner extends Person {
 	private static final long serialVersionUID = 6058308647075194395L;
 	
 	private Set<Pet> pets;
+	private String address;
+	private String city;
+	private String telephone;
+	
 
 	public Set<Pet> getPets() {
 		return pets;
@@ -20,6 +24,30 @@ public class Owner extends Person {
 
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
 	}
 	
 }
